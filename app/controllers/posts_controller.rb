5c07c8528187729221d39513e8bc6c22cd7fedf9
daichan0428 <daichan0428@hotmail.com>
2020-03-20 14:51:01 +0900

    add edit destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   
+  def index
+    @posts = Post.order(created_at: :desc)
+  end
+
   def new
     @post = Post.new # フォーム用の空のインスタンスを生成する。
   end
@@ -16,6 +20,23 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました。"
+  end
 
 
   private
