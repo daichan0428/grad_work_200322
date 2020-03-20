c2fddb608ccecc37cf066ee1cebd1c84d645ff52
daichan0428 <daichan0428@hotmail.com>
2020-03-20 12:16:27 +0900

    add flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,25 @@
 class PostsController < ApplicationController
+  
+  def new
+    @post = Post.new # フォーム用の空のインスタンスを生成する。
+  end
+  def create
+    @post = Post.new(post_params) # ストロングパラメータを引数に
+    @post.save # saveをしてデータベースに保存する。
+    redirect_to @post, notice: '投稿を保存しました' # showページにリダイレクト
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+
+  private
+  def post_params # ストロングパラメータを定義する
+    params.require(:post).permit(:caption)
+  end
+
+
 
 end
